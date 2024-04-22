a014e7e9b6ec2879e491c0a15bc8793aef58dcc2
the-map003 <aimepatrick2003@gmail.com>
2024-04-22 15:01:46 +0200

    changes

--- GSM/accounts/urls.py
+++ GSM/accounts/urls.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-from django.urls import path
-from . import views
 
-urlpatterns = [
-    path("", views.account, name="account"),
-]
-=======
 from django.contrib import admin
 from django.urls import path,include
 from.import views
@@ -14,4 +7,3 @@ urlpatterns = [
     path('',views.login,name='login'),
     
 ]
->>>>>>> bb170f86388bbd95d77827c257d7c2d2c71e5855
--- GSM/accounts/views.py
+++ GSM/accounts/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
-
-
-def account(request):
-    return render(request, "accounts/index.html")
-=======
 # Create your views here.
 def login(request):
     
     return render('accounts/login.html')
->>>>>>> bb170f86388bbd95d77827c257d7c2d2c71e5855
+
