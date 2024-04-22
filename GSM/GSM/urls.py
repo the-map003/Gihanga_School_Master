1509f2b683eb77f96c4cbffbe4775da5bd46daa1
Gilbert Byiringiro <143619929+emmgJules@users.noreply.github.com>
2024-04-22 13:24:04 +0200

    routing

--- GSM/GSM/urls.py
+++ GSM/GSM/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('accounts.urls')),
+    path('announcements/',include('announcements_messaging.urls')),
+    path('attendance/',include('attendance_management.urls')),
+    path('fee/',include('fees_management.urls')),
+    path('hardware/',include('fees_management.urls')),
+    path('library/',include('library_management.urls')),
+    path('report/',include('result_report_management.urls')),
+    path('stock/',include('stock_management.urls')),
+    path('student/',include('student_management.urls')),
+    path('playground/',include('student_playground.urls')),
+    path('timetable/',include('timetable_management.urls')),
 ]
