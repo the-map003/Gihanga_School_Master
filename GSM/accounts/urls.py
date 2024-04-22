461b7b06f6449cfc6907505b2ecc61bc2d0eeaf6
the-map003 <aimepatrick2003@gmail.com>
2024-04-22 16:57:34 +0200

    yeah

--- GSM/accounts/urls.py
+++ GSM/accounts/urls.py
@@ -4,5 +4,7 @@ from django.urls import path,include
 from.import views
 
 urlpatterns = [
-    path('', views.login,name='login'),
+    path('login/', views.login, name='login'),
+    path('redirect/',views.redirect_users , name='redirect_based_on_role'),
+    # Add other URL patterns for your apps
 ]
