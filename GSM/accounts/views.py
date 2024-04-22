09889eb55eb79913b260f05db95de72c6df63371
Gilbert Byiringiro <143619929+emmgJules@users.noreply.github.com>
2024-04-22 18:05:45 +0200

    ffdg

--- GSM/accounts/views.py
+++ GSM/accounts/views.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
@@ -17,16 +17,6 @@ def login(request):
             return render(request, 'accounts/login.html', {'error_message': 'Invalid username or password'})
     else:
         return render(request, 'accounts/login.html')
-=======
-from django.shortcuts import render
-from django.shortcuts import redirect
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import views as auth_views
-from .models import Users
->>>>>>> 418191de5a02d4eb6bbcd0133eaf3bbc70c83c04
-
-def login(request): 
-    return render(request, 'accounts/login.html')
 
 @login_required
 def redirect_users(request):
