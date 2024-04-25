8af71e746d8b55d4323cd37a0a7f1432d0abdfa5
the-map003 <aimepatrick2003@gmail.com>
2024-04-25 09:47:54 +0200

    recieved

--- GSM/student_management/admin.py
+++ GSM/student_management/admin.py
@@ -4,6 +4,4 @@ from .models import *
 # Register your models here.
 admin.site.register(Class)
 admin.site.register(Student)
-admin.site.register(Pages)
-admin.site.register(Pnotice)
-admin.site.register(Notice)
+admin.site.register(Teacher)
